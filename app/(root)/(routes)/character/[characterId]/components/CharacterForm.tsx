5e9ea13e2620ac75bfb57a8c02bb6312a4888612
Emanuel Garcia <wemagw@gmail.com>
2023-09-04 16:27:31 -0300

    Form finished

--- app/(root)/(routes)/character/[characterId]/components/CharacterForm.tsx
+++ app/(root)/(routes)/character/[characterId]/components/CharacterForm.tsx
@@ -4,12 +4,31 @@ import * as z from "zod";
 import { Category, Character } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Wand2 } from "lucide-react";
 
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import ImageUpload from "@/components/ImageUpload";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
+
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 
 
 interface CharacterFormProps {
@@ -166,10 +185,72 @@ const CharacterForm = ({
                   <FormDescription>
                     Select a category for your AI Character
                   </FormDescription>
+                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium">
+                Configuration
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                Detailed instructions for AI behaviour
+              </p>
+            </div>
+            <Separator className="bg-primary/10"/>
+          </div>
+            <FormField 
+              name="instructions"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Instructions</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="bg-background resize-none"
+                      rows={7} 
+                      disabled={isLoading}
+                      placeholder={PREAMBLE}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Describe in detail your character&apos;s backstory and relevant details.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField 
+              name="seed"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Example Conversation</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="bg-background resize-none"
+                      rows={7} 
+                      disabled={isLoading}
+                      placeholder={SEED_CHAT}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Describe in detail your character&apos;s backstory and relevant details.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="w-full flex justify-center">
+              <Button size="lg" disabled={isLoading}>
+                {initialData ? "Edit your character" : "Create your character"}
+                <Wand2 className="w-4 h-4 ml-2"/>
+              </Button>
+            </div>
         </form>
       </Form>
     </div>
