1151884d27d8e0814b5bc1564f8d73239ad16176
Emanuel Garcia <wemagw@gmail.com>
2023-09-11 13:24:12 -0300

    character list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,14 +2,44 @@ import prismadb from "@/lib/prismadb";
 
 import SearchInput from "@/components/SearchInput";
 import Categories from "@/components/Categories";
+import Characters from "@/components/Characters";
 
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  }
+}
 
-const RootPage = async () => {
+const RootPage = async ({
+  searchParams
+}: RootPageProps) => {
+
+  const data = await prismadb.character.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true
+        }
+      }
+    }
+  })
+
   const categories = await prismadb.category.findMany()
   return (
     <div className="h-full p-4 space-y-2">
         <SearchInput />
         <Categories data={categories} />
+        <Characters data={data} />
     </div>
   )
 }
